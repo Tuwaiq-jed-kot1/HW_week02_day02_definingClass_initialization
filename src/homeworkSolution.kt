f232deb79a8ececd6c294909b3dde8c6094f3a17
badrFadul <badr.fadul.jed.k1@tuwaiq.edu.sa>
2021-10-04 21:57:43 +0300

    good hw

--- src/homeworkSolution.kt
+++ src/homeworkSolution.kt
@@ -1,8 +1,31 @@
+class Car(var name: String, var manufacturing_year: Int) {
 
+    lateinit var color: String
 
+    constructor(name2: String, manufacturing_year2: Int, color2: String) : this(
+        name = name2,
+        manufacturing_year = manufacturing_year2
+    ) {
+        color = color2
+    }
 
+    init {
+        println("hello, nice $name vehicle \n ")
+    }
 
+    fun accelerate() {
+        println("*the car speed increase*\n")
+    }
+
+    fun carBrake() {
+        println("*the car speed decrees*\n")
+    }
+}
 
 fun main() {
+    val ahmed = Car("Mercedes", 1998)
+    val mohammed = Car("GMC", 2008, "red")
 
+    ahmed.accelerate()
+    mohammed.carBrake()
 }

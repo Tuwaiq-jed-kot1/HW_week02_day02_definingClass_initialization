f7b6967972df18a63ccb045646fe6d05fedc85c1
MahilaAlmutairi <Mahila.Almutairi.jed.k1@tuwaiq.edu.sa>
2021-10-04 19:33:30 +0300

    HW2_W2

--- src/homeworkSolution.kt
+++ src/homeworkSolution.kt
@@ -1,8 +1,28 @@
+class car(var name: String, var manufacturingYear: Int) {
+    constructor(name: String, manufacturingYear: Int, color: String) : this(
+        name = name,
+        manufacturingYear = manufacturingYear
+    ) {
 
+    }
 
+    private var breake = false
+    private var accelerator = false
 
+    init {
+        println(" Welcome $name !!")
+    }
 
+    fun useBreake() {
+        breake = true
+    }
+
+    fun accelerateand() {
+        accelerator = true
+    }
+}
 
 fun main() {
+val tesla=car("Tesla",2021)
 
 }

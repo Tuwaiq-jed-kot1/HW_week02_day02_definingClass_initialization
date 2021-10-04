0b7b2f03f3faa8385c141688844cb919a972293e
RanaJaberAlrajhi <Rana.Alrajhi.jed.k1@tuwaiq.edu.sa>
2021-10-05 00:07:25 +0300

    homework solution

--- src/homeworkSolution.kt
+++ src/homeworkSolution.kt
@@ -1,8 +1,30 @@
 
+class Car constructor(var name:String, var year:Int) {
 
+    lateinit var cColor1: String
 
+    lateinit var Action: String
 
+    constructor(cName: String, cColor: String, cYear: Int) : this(name = cName, year = cYear) {
+        cColor1 = cColor
+        Action = cName
+    }
+
+    init {
+        println("Welcome in your $name $year car")
+    }
+
+    fun accelerator(){
+        println("$Action accelerator is On")
+    }
+
+    fun brake(){
+        println("$Action brake is On")
+    }}
 
 fun main() {
-
+    val bmw : Car = Car ("bmw","black", 1993)
+    println("The car color is black")
+    bmw.accelerator()
+    bmw.brake()
 }

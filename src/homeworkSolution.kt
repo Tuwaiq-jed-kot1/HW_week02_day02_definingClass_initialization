58976332f958719ac8c268f4241e9e1426cb4522
musab <mas.gh123@hotmail.com>
2021-10-04 23:05:33 +0300

    Good

--- src/homeworkSolution.kt
+++ src/homeworkSolution.kt
@@ -1,8 +1,29 @@
+//## Part II - writing code.
+//write a kotlin program with car class that has three property name and color and manufacturing year
+//use the primary constructor to ask for the name and manufacturing year and use the secondary constructor
+// to ask for the name and manufacturing year and color
+//and print a welcoming massage after creating new object from the class that contains the car name
+//and give the car ability to accelerateand the ability to use breake
+class Car(carName: String, manufacYear: Int){
+    lateinit var color: String
+    lateinit var carModel: String
+    constructor(cName: String, cColor: String, cManufacYear: Int): this(carName = cName, manufacYear = cManufacYear){
+        color = cColor
+        carModel = cName
+    }
+    init {
+        println("Hello there!! Your car model is $carName")
+    }
+    fun accelerate(){
+        println("$carModel is accelerating")
+    }
+    fun useBrakes(){
+        println("$carModel is slowing down")
+    }
+}
 
-
-
-
-
-fun main() {
-
+fun main(){
+    val myCar: Car = Car("Lamborghini Urus", "Yellow", 2021)
+    myCar.accelerate()
+    myCar.useBrakes()
 }

d63deba076ab74837039098ef5cea9b0d96cc442
SuzanneSaleem1 <Suzan.Alluhaybi.jed.k1@tuwaiq.edu.sa>
2021-10-05 08:42:35 +0300

    homework

--- src/homeworkSolution.kt
+++ src/homeworkSolution.kt
@@ -1,8 +1,29 @@
+class Car(cName:String,manufacturingYear:Int) {
 
+      private var cName:String ="bmw"
+     var manufacturingYear:Int =2000
 
+    constructor (name: String, maYear: Int, color: String) : this() {
+        cName=name
+        manufacturingYear=maYear
+        var carColor = color
+    }
+    var carSpeed = 1
+    var accelerate = 1
+    var breakcar=0
 
+    fun accelerate() {
+        println("car accelerate = $carSpeed + $accelerate")
+    }
 
+    fun Break() {
 
-fun main() {
+    println("car accelerate = $carSpeed- $breakcar")
+}
+    }
 
+fun main(){
+    var welcome = Car("Toyota", 2007, "blue")
+    println("Hello, welcome $welcome")
+    Car().accelerate()
 }
